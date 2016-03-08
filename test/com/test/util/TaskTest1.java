42a63c567a40bcbbb8e3d0f6facc04457f47cc7a
LXC <798196071@qq.com>
2016-03-08 09:37:54 +0800

    测试套件
    测试套件

---
+++ test/com/test/util/TaskTest1.java
@@ -0,0 +1,14 @@
+package com.test.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TaskTest1 {
+
+	@Test
+	public void test() {
+		System.out.println("thins is test1...");
+	}
+
+}
---
+++ test/com/test/util/TaskTest2.java
@@ -0,0 +1,13 @@
+package com.test.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TaskTest2 {
+
+	@Test
+	public void test() {
+		System.out.println("thins is test2...");
+	}
+}
